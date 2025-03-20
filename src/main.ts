4d74d227bc5455d6720dc7c7cd48e12ea11d5842
Josué Bayidikila <josue-dev@outlook.fr>
2025-03-20 12:50:10 +0100

    feat: ajout de la configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Odyssey Gate API')
+    .setDescription("Documentation de l'API pour l'application Odyssey Gate")
+    .setVersion('1.0')
+    // La configuration du Bearer Token
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'access-token' // nom de la stratégie (sera utilisé dans les décorateurs que j'ai mis)
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // Et la redirection de la racine vers Swagger
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/api-docs');
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
